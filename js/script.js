ecfc9928884f6e6ee2790bd361827f03b6bc2af2
powerad23 <powerad23@gmail.com>
2015-09-25 21:08:47 -0700

    Added display but still funky

--- js/script.js
+++ js/script.js
@@ -1,57 +1,69 @@
 (function () {
 	var calcDisplay = [];
-	var space = ""
-
+	var screenDisplay = [];
 	function buttonPress(input) {
 		calcDisplay.push(input);
-		//calcDisplay.push(space)
 		console.log(calcDisplay);
 	}
 	$("button#1.number").click(function () {
 		buttonPress(1);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#2.number").click(function () {
 		buttonPress(2);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#3.number").click(function () {
 		buttonPress(3);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#4.number").click(function () {
 		buttonPress(4);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#5.number").click(function () {
 		buttonPress(5);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#6.number").click(function () {
 		buttonPress(6);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#7.number").click(function () {
 		buttonPress(7);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#8.number").click(function () {
 		buttonPress(8);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#9.number").click(function () {
 		buttonPress(9);
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#10.number").click(function () {
 		buttonPress("+");
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#11.number").click(function () {
 		buttonPress("-");
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#12.number").click(function () {
 		buttonPress("/");
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#13.number").click(function () {
 		buttonPress("*");
+		$("<p>"+ calcDisplay+"</p>").appendTo('#output')
 	});
 	$(".number#14.number").click(function () {
 		var compute = calcDisplay.join("");
-		console.log(eval(compute));
+		$("<p>"+ eval(compute)+"</p>").appendTo('#output')
 	});
 	$(".number#15.number").click(function () {
 		calcDisplay = []
+		$("#output").empty();
 	});
-	$("h4").append(calcDisplay);
+	
 })();
