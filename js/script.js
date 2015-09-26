8feb541ccd359e1b42319f097a35431c62301b2d
powerad23 <powerad23@gmail.com>
2015-09-25 19:20:28 -0700

    second commit to fix class names

--- js/script.js
+++ js/script.js
@@ -5,31 +5,31 @@ function buttonPress(input) {
 	calcDisplay.push(space)
 	console.log(calcDisplay);
 }
-$('.button1').click(function() {
+$(".number1").click(function() {
 	buttonPress(1);
 });
-$('.button2').click(function() {
+$(".number2").click(function() {
 	buttonPress(2);
 });
-$('.button3').click(function() {
+$(".number3").click(function() {
 	buttonPress(3);
 });
-$('.button4').click(function() {
+$(".number4").click(function() {
 	buttonPress(4);
 });
-$('.button5').click(function() {
+$(".number5").click(function() {
 	buttonPress(5);
 });
-$('.button6').click(function() {
+$(".number6").click(function() {
 	buttonPress(6);
 });
-$('.button7').click(function() {
+$(".number7").click(function() {
 	buttonPress(7);
 });
-$('.button8').click(function() {
+$(".number8").click(function() {
 	buttonPress(8);
 });
-$('.button9').click(function() {
+$(".number9").click(function() {
 	buttonPress(9);
 });
 $("h4").append(calcDisplay);
